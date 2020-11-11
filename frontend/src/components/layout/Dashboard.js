9deee3056739e8114c6aa94571329020a9bcec79
ragokan <okanyldrmop@gmail.com>
2020-11-11 14:00:57 +0300

    Profile on dashboard

--- frontend/src/components/layout/Dashboard.js
+++ frontend/src/components/layout/Dashboard.js
@@ -1,9 +1,34 @@
-import React from "react"
+import React, { useContext } from "react"
+import { AuthContext } from "../../context/AuthContext"
 
 const Dashboard = () => {
+  const { user } = useContext(AuthContext)
+
   return (
     <>
-      <h1>Dashboard</h1>
+      {user ? (
+        <div className="card deep-orange hoverable">
+          <br />
+          <h3 className="center white-text">Your Profile</h3>
+          <div className="card-content white-text">
+            <span className="card-title">{user.fullname}</span>
+            <p>{user.email}</p>
+          </div>
+          <br />
+        </div>
+      ) : (
+        <div>
+          <div className="card deep-orange hoverable">
+            <br />
+            <div className="card-content white-text">
+              <span className="card-title">
+                If you log in, you will see your profile here!
+              </span>
+            </div>
+            <br />
+          </div>
+        </div>
+      )}
     </>
   )
 }
