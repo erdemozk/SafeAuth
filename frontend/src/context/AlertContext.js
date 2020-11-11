79d5b16c26f71d4f7c1562ff54b077f8bd709231
ragokan <okanyldrmop@gmail.com>
2020-11-11 20:42:41 +0300

    useeffect

--- frontend/src/context/AlertContext.js
+++ frontend/src/context/AlertContext.js
@@ -1,14 +1,29 @@
-import React, { createContext, useState } from "react"
-import { newAlert } from "./actions/AlertActions"
+import React, { createContext, useEffect, useState } from "react"
+import alertObject from "./utils/alertObject"
 
 export const AlertContext = createContext()
 
 const AlertContextProvider = (props) => {
   const [alerts, setAlerts] = useState([])
+  let lastAlertID
 
   function addAlert(message, color) {
     newAlert(message, color, setAlerts, alerts)
   }
+
+  const newAlert = (message, color, setAlerts, alerts) => {
+    const alert = alertObject(message, color)
+    lastAlertID = alert.id
+    setAlerts([...alerts, alert])
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      setAlerts(
+        alerts.filter((currentAlert) => currentAlert.id !== lastAlertID)
+      )
+    }, 2500)
+  }, [alerts, lastAlertID])
 
   return (
     <AlertContext.Provider value={{ alerts, addAlert }}>
--- frontend/src/context/actions/AlertActions.js
+++
@@ -1,7 +0,0 @@
-import alertObject from "../utils/alertObject"
-const newAlert = (message, color, setAlerts, alerts) => {
-  const alert = alertObject(message, color)
-  setAlerts([...alerts, alert])
-}
-
-export { newAlert }
