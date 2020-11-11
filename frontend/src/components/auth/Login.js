dd2228d2ed900263bcbc69e04c951bcfcba1129a
ragokan <okanyldrmop@gmail.com>
2020-11-11 10:16:23 +0300

    forms

--- frontend/src/components/auth/Login.js
+++ frontend/src/components/auth/Login.js
@@ -1,10 +1,52 @@
-import React from "react"
+import React, { useState } from "react"
 
 const Login = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const loginForm = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <>
-      <h1>Login Page</h1>
-    </>
+    <div>
+      <br />
+      <div className="row">
+        <form className="col s12 center" onSubmit={loginForm}>
+          <div className="row">
+            <div className="input-field col s12">
+              <i className="material-icons prefix">email</i>
+              <input
+                id="email"
+                type="email"
+                className="validate"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <label htmlFor="email">Email</label>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="input-field col s12">
+              <i className="material-icons prefix">lock</i>
+              <input
+                id="password"
+                type="password"
+                className="validate"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <label htmlFor="password">Password</label>
+            </div>
+          </div>
+
+          <button className="waves-effect waves-light btn">
+            <i className="material-icons right">login</i>Login
+          </button>
+        </form>
+      </div>
+    </div>
   )
 }
 
--- frontend/src/components/auth/Register.js
+++ frontend/src/components/auth/Register.js
@@ -1,10 +1,78 @@
-import React from "react"
+import React, { useState } from "react"
 
 const Register = () => {
+  const [firstName, setFirstName] = useState("")
+  const [lastName, setLastName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const registerForm = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <>
-      <h1>Register Page</h1>
-    </>
+    <div>
+      <br />
+      <div className="row">
+        <form className="col s12 center" onSubmit={registerForm}>
+          <div className="row">
+            <div className="input-field col s6">
+              <i className="material-icons prefix">person</i>
+              <input
+                id="first_name"
+                type="text"
+                className="validate"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+              />
+              <label htmlFor="first_name">First Name</label>
+            </div>
+            <div className="input-field col s6">
+              <input
+                id="last_name"
+                type="text"
+                className="validate"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+              />
+              <label htmlFor="last_name">Last Name</label>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="input-field col s12">
+              <i className="material-icons prefix">email</i>
+              <input
+                id="email"
+                type="email"
+                className="validate"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <label htmlFor="email">Email</label>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="input-field col s12">
+              <i className="material-icons prefix">lock</i>
+              <input
+                id="password"
+                type="password"
+                className="validate"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <label htmlFor="password">Password</label>
+            </div>
+          </div>
+
+          <button className="waves-effect waves-light btn">
+            <i className="material-icons right">send</i>Register
+          </button>
+        </form>
+      </div>
+    </div>
   )
 }
 
