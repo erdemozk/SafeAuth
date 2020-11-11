bebab415ac0e1ddb861d5458a8d46d7b39aecca3
ragokan <okanyldrmop@gmail.com>
2020-11-11 21:16:58 +0300

    update

--- frontend/src/context/AlertContext.js
+++ frontend/src/context/AlertContext.js
@@ -1,29 +1,14 @@
-import React, { createContext, useEffect, useState } from "react"
-import alertObject from "./utils/alertObject"
+import React, { createContext, useState } from "react"
+import { newAlert } from "./actions/AlertActions"
 
 export const AlertContext = createContext()
 
 const AlertContextProvider = (props) => {
   const [alerts, setAlerts] = useState([])
-  let lastAlertID
 
   function addAlert(message, color) {
     newAlert(message, color, setAlerts, alerts)
   }
-
-  const newAlert = (message, color, setAlerts, alerts) => {
-    const alert = alertObject(message, color)
-    lastAlertID = alert.id
-    setAlerts([...alerts, alert])
-  }
-
-  useEffect(() => {
-    setTimeout(() => {
-      setAlerts(
-        alerts.filter((currentAlert) => currentAlert.id !== lastAlertID)
-      )
-    }, 2500)
-  }, [alerts, lastAlertID])
 
   return (
     <AlertContext.Provider value={{ alerts, addAlert }}>
