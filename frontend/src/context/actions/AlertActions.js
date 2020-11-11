1b45c251bda8b82dd71f047b1b015327fc135ae3
ragokan <okanyldrmop@gmail.com>
2020-11-11 20:34:51 +0300

    no need for async in alerts

--- frontend/src/context/actions/AlertActions.js
+++ frontend/src/context/actions/AlertActions.js
@@ -1,11 +1,7 @@
 import alertObject from "../utils/alertObject"
-const newAlert = async (message, color, setAlerts, alerts) => {
-  const alert = await alertObject(message, color)
-  await setAlerts([...alerts, alert])
-
-  setTimeout(() => {
-    setAlerts(alerts.filter((currentAlert) => currentAlert.id !== alert.id))
-  }, 5000)
+const newAlert = (message, color, setAlerts, alerts) => {
+  const alert = alertObject(message, color)
+  setAlerts([...alerts, alert])
 }
 
 export { newAlert }
