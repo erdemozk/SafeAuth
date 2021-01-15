0bccf3e5b1a49cde64774d4c4492699ecd7b0213
Rago <63511519+ragokan@users.noreply.github.com>
2021-01-15 20:27:49 +0300

    change to json

--- backend/routes/Register.js
+++ backend/routes/Register.js
@@ -1,27 +1,27 @@
-import express from "express"
-import User from "../models/User.js"
-const router = express.Router()
+import express from "express";
+import User from "../models/User.js";
+const router = express.Router();
 
-import registerValidation from "../validation/registerValidation.js"
+import registerValidation from "../validation/registerValidation.js";
 
 router.post("/", async (req, res) => {
   // Validate The Request
-  const { error } = registerValidation(req.body)
-  if (error) return res.status(400).send(error.details[0].message)
+  const { error } = registerValidation(req.body);
+  if (error) return res.status(400).json(error.details[0].message);
   try {
-    let { email, password, fullname } = req.body
+    let { email, password, fullname } = req.body;
 
     // Check if user exists
-    const userCheck = await User.findOne({ email })
-    if (userCheck) return res.status(400).json("User already exists!")
+    const userCheck = await User.findOne({ email });
+    if (userCheck) return res.status(400).json("User already exists!");
 
-    const user = new User({ email, password, fullname })
-    await user.save()
-    const token = user.getToken()
-    res.status(201).json(token)
+    const user = new User({ email, password, fullname });
+    await user.save();
+    const token = user.getToken();
+    res.status(201).json(token);
   } catch (err) {
-    return res.status(400).json("Error :" + err)
+    return res.status(400).json("Error :" + err);
   }
-})
+});
 
-export default router
+export default router;
